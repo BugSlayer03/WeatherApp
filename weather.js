e5dd0ea7b60652d7fba08f016a242b4f75dba396
Priyanshu Patel <priyanshuprpatel@gmail.com>
2026-03-18 00:39:39 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,58 @@
+// http://api.weatherapi.com/v1/current.json?key=0cf3f4985d3e404a908154315261703&q=Varanasi&aqi=no
+
+const tempField = document.querySelector('.temp');
+const locationField = document.querySelector('.location');
+const dateField = document.querySelector('.date');
+const conditionText = document.querySelector('.condition p');
+const minmaxField = document.querySelector('.min_max');
+const searchField = document.querySelector('.city_input');
+const condImg = document.querySelector('.condImg');
+const windSpeedField = document.querySelector('.wind .w1');
+const lastUpdateField=document.querySelector('.lastUpdated');
+// const windDirField=document.querySelector('.wind .w2');
+
+let target = 'Varanasi';
+
+const fetchResults = async (targetLocation) => {
+    let url = `http://api.weatherapi.com/v1/current.json?key=0cf3f4985d3e404a908154315261703&q=${targetLocation}&aqi=no`;
+
+    const res = await fetch(url);
+
+    const data = await res.json();
+
+    console.log(data);
+
+    let locationName = data.location.name;
+    console.log(locationName);
+    let locationTime = data.location.localtime;
+    console.log(locationTime);
+    let locationTemp = data.current.temp_c;
+    console.log(locationTemp);
+    let locationCond = data.current.condition.text;
+    console.log(locationCond);
+    let locationWind = data.current.wind_kph;
+    console.log(locationWind);
+    // let windDir=data.current.wind_dir;
+    let lastUpdate=data.current.last_updated;
+
+    tempField.innerText = `${locationTemp}°C`;
+    locationField.innerText = locationName;
+    // dateField.innerText=locationTime;
+    let dateObj = new Date(locationTime);
+    dateField.innerText = dateObj.toDateString();
+    conditionText.innerText = locationCond;
+    condImg.src = "https:" + data.current.condition.icon;
+    windSpeedField.innerText = 'Wind Speed (Kph) : ' + locationWind;
+    lastUpdateField.innerText='Last Updated : '+lastUpdate;
+    // windDirField.innerText=windDir;
+
+}
+
+searchField.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        target = searchField.value;
+        fetchResults(target);
+    }
+});
+
+fetchResults(target);
