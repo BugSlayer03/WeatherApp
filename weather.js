a6fbf33605422204a2f55be20a59bdf3856621c3
Priyanshu Patel <priyanshuprpatel@gmail.com>
2026-03-19 08:44:21 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -11,6 +11,12 @@ const windSpeedField = document.querySelector('.wind .w1');
 const lastUpdateField=document.querySelector('.lastUpdated');
 // const windDirField=document.querySelector('.wind .w2');
 
+const countryAbbreviations = {
+    "United States of America": "USA",
+    "United Arab Emirates": "UAE",
+    "United Kingdom": "UK",
+};
+
 let target = 'Varanasi';
 
 const fetchResults = async (targetLocation) => {
@@ -25,6 +31,9 @@ const fetchResults = async (targetLocation) => {
     let locationName = data.location.name;
     console.log(locationName);
     let locationCountry=data.location.country;
+    if (countryAbbreviations[country]) {
+        country = countryAbbreviations[country];
+    }
     let locationTime = data.location.localtime;
     console.log(locationTime);
     let locationTemp = data.current.temp_c;
